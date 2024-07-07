686cb84f93e2ef8a0dba70ebd8fd67daa59538a7
RoslanLibzo <roslanlibzo@gmail.com>
2024-07-07 11:53:06 +0100

    Added home / login / welcome files in pages

---
+++ app/resources/pages/home.php
@@ -0,0 +1,2 @@
+<h1>Here is the homepage!</h1>
+<p>Make sure to visit /login or /welcome to see more.</p>
---
+++ app/resources/pages/login.php
@@ -0,0 +1,19 @@
+<?php
+
+;?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+</head>
+<body>
+    <div class="app">
+        <h1 class="title">Here is the Login</h1>
+        
+    </div>
+</body>
+</html>
+<h1>HELLO!</h1>
